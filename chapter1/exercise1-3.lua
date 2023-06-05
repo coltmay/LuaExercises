7b8d7df307547a2ed68e33bd07359f82b547654a
Colten Mayberry <colten_mayberry@pm.me>
2023-06-05 07:06:53 -0500

    finished exercise 1.3, more trivia than coding

---
+++ chapter1/exercise1-3.lua
@@ -0,0 +1,8 @@
+--[[ ===============================================================================================
+Exercise 1.3: Can you name other languages that use "--" for comments?
+================================================================================================= ]]
+
+-- SQL is the first language that comes to mind.  At least as far as what I use day-to-day.
+-- Looking further into it it seems as though the following also follow this syntax for inline commenting:
+-- AppleScript
+-- Haskell
