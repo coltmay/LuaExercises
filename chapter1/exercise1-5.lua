41d6ba1f2b48509409748cff50a7f3b09235e05d
Colten Mayberry <colten_mayberry@pm.me>
2023-06-07 19:56:44 -0500

    finished exercise 1.5, more knowledge based questions

---
+++ chapter1/exercise1-5.lua
@@ -0,0 +1,9 @@
+--[[ ===============================================================================================
+Exercise 1.5: What is the value of the expression type(nil) == nil? (You can use Lua to check
+your answer.) Can you explain this result?
+================================================================================================= ]]
+
+--[[ ===============================================================================================
+The value of the expression is `false`, due to the fact that type(nil) we result in a string,
+not the actual value of nil.
+================================================================================================= ]]
